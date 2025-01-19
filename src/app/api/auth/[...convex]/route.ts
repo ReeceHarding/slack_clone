b2d30c5eaa518cf5594771b64ab6b416f17c5905
Reece Harding <reeceharding@Reeces-MacBook-Air.local>
2025-01-19 15:26:40 -0700

    fix: update auth implementation to use Clerk with Convex

--- src/app/api/auth/[...convex]/route.ts
+++ src/app/api/auth/[...convex]/route.ts
@@ -1,6 +1,25 @@
-import { convexAuthNextjsMiddleware } from "@convex-dev/auth/nextjs/server";
+import { ConvexHttpClient } from "convex/browser";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse, NextRequest } from "next/server";
 
-const handler = convexAuthNextjsMiddleware();
+export async function GET(request: NextRequest) {
+  const auth = getAuth(request);
+  const token = await auth.getToken({ template: "convex" });
+  return new NextResponse(JSON.stringify({ token }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
 
-export const GET = handler;
-export const POST = handler; 
+export async function POST(request: NextRequest) {
+  const auth = getAuth(request);
+  const token = await auth.getToken({ template: "convex" });
+  return new NextResponse(JSON.stringify({ token }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+} 
